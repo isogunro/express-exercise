d52a5a816e5d1ad7268fcde90a66a8d78bd4b7cf
Steve Jobs <steve@apple.com>
2018-03-22 14:23:24 -0400

    fixed flash issue

--- app.js
+++ app.js
@@ -41,12 +41,12 @@ app.use(session({cookie: { maxAge: 60000 }}));
 app.use(flash());
 
 //Global variables
-/*app.use(function(req, res, next){
-    res.local.success_msg = req.flash('success_msg');
-    res.local.error_msg = req.flash('error_msg');
+app.use(function(req, res, next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
     next();
-})*/
+})
 
 //Index Route
 app.get('/', (req, res)=>{
