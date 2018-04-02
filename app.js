c0b8c77ec4a407373ff432c30016817872f98f90
Steve Jobs <steve@apple.com>
2018-04-02 07:11:40 -0400

    changes made from following udemy course

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override'); //dynamically overrides post when it's time to PUT (update)
 const flash = require('connect-flash');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -13,31 +15,46 @@ const app = express();
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
 
+//Passport locals
+require('./config/passport')(passport);
+
+//Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
 //connect to mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev')
 .then(()=>console.log('MongoDB Conneced....'))
 .catch(err => console.log(err));
 
-//handlebars Middleware
+//handlebars Middleware---------------------------------------------------
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 
 app.set('view engine', 'handlebars');
+//handlebars Middleware---------------------------------------------------
+
 
 
 //Body parser middleware - allows us to access whatever is submitted from the form of the request object
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//Body parser middleware - allows us to access whatever is submitted from the form of the request object
 
-//method Override middleware
+
+//method Override middleware (//dynamically overrides post when it's time to PUT (update))
 app.use(methodOverride('_method'));
+//method Override middleware (//dynamically overrides post when it's time to PUT (update))
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+//Static folder
 
 
-
-
+//cookie parser
 app.use(cookieParser('secret'));
 app.use(session({cookie: { maxAge: 60000 }}));
+
 app.use(flash());
 
 //Global variables
